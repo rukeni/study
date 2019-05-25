ce784090d08195637ecc02c7519252d8df8e9495
rukeni <rukeni@naver.com>
2019-05-26 00:19:17 +0900

    onChange

--- react-counter/src/components/Counter.jsx
+++ react-counter/src/components/Counter.jsx
@@ -4,9 +4,15 @@ function Counter() {
   
   const body = document.body
 
-  const [count, setCount] = React.useState(0)
+  let [count, setCount] = React.useState(0)
+  const [blue, setBlue] = React.useState(0)
+  const [red, setRed] = React.useState(0)
+  const [green, setGreen] = React.useState(0)
   const color = {
     color: count<0 ? 'red' : 'blue'
+  }
+  const background = {
+    backgroundColor: `rgb(${red},${green},${blue})`
   }
   const onIncrement = (e) => {
     e.preventDefault()
@@ -17,6 +23,12 @@ function Counter() {
     e.preventDefault()
     setCount(count-1)
   }
+
+  const onChange = (e) => {
+    e.preventDefault()
+      setCount(count = e.target.value) 
+    
+  }
   // let blue = 0
   // let green = 0
   // let red = 0
@@ -26,7 +38,7 @@ function Counter() {
     <div className="Counter">
       <div id="react-highlight" style={color}>react</div>
       <label>숫자 입력 값 : <h1 style={color}>{count}</h1> </label>
-      <input value={count} ref={input} type="text"/>
+      <input value={count} onChange={onChange} type="text"/>
       <button onClick={onIncrement}>Increment</button>
       <button onClick={onDecrement}>Decrement</button>
       {() => {
